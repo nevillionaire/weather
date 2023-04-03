109ce7d5eb9c10aef5c122cd3d154c924964a22c
nevillionaire <nevillepeter12@gmail.com>
2023-04-03 19:38:19 +0300

    beta version

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '287e7f6474msh149ca08332367d4p1b4433jsn472390b39d6a',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
