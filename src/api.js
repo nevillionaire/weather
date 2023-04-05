bf6d20540bdd450ecc6301c537ace86d2c6b69a5
nevillionaire <nevillepeter12@gmail.com>
2023-04-05 11:39:07 +0300

    tests

--- src/api.js
+++ src/api.js
@@ -1,11 +1,10 @@
 export const geoApiOptions = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '287e7f6474msh149ca08332367d4p1b4433jsn472390b39d6a',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "287e7f6474msh149ca08332367d4p1b4433jsn472390b39d6a",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
 };
-
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
